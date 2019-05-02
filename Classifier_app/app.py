4da34fad633bbf76d1fc5a20efaca0489eab073e
mankek <krystal.manke@gmail.com>
2019-05-02 13:04:20 -0500

    it works + readme

--- Classifier_app/app.py
+++ Classifier_app/app.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python
 from flask import Flask, render_template, request
 import numpy
+import pickle
+import os
 
 app = Flask(__name__)
+
+clf_dir = r"..\Spam_classifier\pkl_objects"
+clf = pickle.load(open(os.path.join(clf_dir, 'classifier.pkl'), 'rb'))
 
 
 @app.route("/", methods=["GET"])
 @app.route("/index", methods=["GET"])
 def index():
-    return render_template("index.html", result=None)
+    return render_template("index.html", label=None, prob=None)
 
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
+    params = []
     input_text = request.form["text"]
-    return render_template("index.html", result="No results yet!")
+    params.extend(find_words(input_text))
+    params.extend(find_chars(input_text))
+    params.extend(find_capitals(input_text))
+    label, probability = classify(params)
+    return render_template("index.html", label=label, prob=probability)
 
 
 def find_words(input_text):
@@ -24,12 +34,13 @@ def find_words(input_text):
                        "font", "000", "money", "hp", "hpl", "george", "650", "lab", "labs", "telnet", "857", "data",
                        "415", "85", "technology", "1999", "parts", "pm", "direct", "cs", "meeting", "original",
                        "project", "re", "edu", "table", "conference"]
-    non_alphanum = ["#", "@", "-", ".", "$", "*", "(", ")", "+", ";", "~", ":", "'", "/", "%", "_", "?", ",", "=", "&", "!"]
+    non_alphanum = ["#", "@", "-", ".", "$", "*", "(", ")", "+", ";", "~", ":", "'", "/", "%", "_", "?", ",", "=", "&",
+                    "!"]
 
     for s in non_alphanum:
         input_text = input_text.split(s)
         input_text = " ".join(input_text)
-    input_text = input_text.split(" ")
+    input_text = input_text.lower().split(" ")
     for t in important_words:
         word_count = input_text.count(t)
         freqs.append(round((word_count/len(input_text))*100, 2))
@@ -44,17 +55,6 @@ def find_chars(input_text):
         freqs.append(round((char_count/len(input_text))*100, 2))
     return freqs
 
-
-def capital_run(input_text):
-    capital_lengths = []
-    length = 0
-    for i in input_text:
-        if not i.islower():
-            length += 1
-        else:
-            capital_lengths.append(length)
-            length = 0
-    return numpy.mean(capital_lengths)
 
 def find_capitals(input_text):
     capitals = []
@@ -73,6 +73,14 @@ def find_capitals(input_text):
     capitals.append(max(capital_lengths))
     capitals.append(len(capital_lengths))
     return capitals
+
+
+def classify(params):
+    params = [params]
+    classes = {1: "spam", 0: "not spam"}
+    y = clf.predict(params)[0]
+    proba = numpy.max(clf.predict_proba(params))
+    return classes[y], str(round(proba * 100, 2))
 
 
 if __name__ == '__main__':
